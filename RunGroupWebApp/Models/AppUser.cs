69a6d25dd37b7ee15bb473f531ba4c91265487a8
Bashar Saab <bashar.saab@gmail.com>
2024-11-07 19:47:10 -0500

    Second day -part 4

--- RunGroupWebApp/Models/AppUser.cs
+++ RunGroupWebApp/Models/AppUser.cs
@@ -1,15 +1,17 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 
 namespace RunGroupWebApp.Models
 {
-    public class AppUser
+    public class AppUser:IdentityUser
     {
-        [Key]
-        public string Id { get; set; }  
+       
         public int? Pace {  get; set; }
         public int? Mileage { get; set; }
-
+        [ForeignKey("Address")]
+        public int AddressId { get; set; }
         public Address? Address { get; set; }
 
         public ICollection<Race> Races { get; set; }
--- RunGroupWebApp/Program.cs
+++ RunGroupWebApp/Program.cs
@@ -1,8 +1,12 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 using RunGroopWebApp.Data;
 using RunGroupWebApp.Data;
 using RunGroupWebApp.Helpers;
 using RunGroupWebApp.Interfaces;
+using RunGroupWebApp.Models;
 using RunGroupWebApp.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,6 +18,12 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 
 });
+builder.Services.AddIdentity<AppUser,IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddMemoryCache();
+builder.Services.AddSession();
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie();
 builder.Services.AddScoped<IClubRepository, ClubRepository>();
 builder.Services.AddScoped<IRaceRepository,RaceRepository>();
 builder.Services.AddScoped<IPhotoService,PhotoService>();
@@ -22,7 +32,8 @@ var app = builder.Build();
 
 if(args.Length == 1 && args[0].ToLower() == "seeddata")
 {
-    Seed.SeedData(app);
+    await Seed.SeedUsersAndRolesAsync(app);
+    //Seed.SeedData(app);
 }
 
 // Configure the HTTP request pipeline.
