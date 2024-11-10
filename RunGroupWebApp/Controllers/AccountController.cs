5079aa93143c9dae11c6e5ae211811c3ad14d77c
Bashar Saab <bashar.saab@gmail.com>
2024-11-09 22:01:35 -0500

    Third day - part 1

--- RunGroupWebApp/Controllers/AccountController.cs
+++ RunGroupWebApp/Controllers/AccountController.cs
@@ -12,7 +12,7 @@ namespace RunGroupWebApp.Controllers
         private readonly SignInManager<AppUser> _signInManager;
         private readonly ApplicationDbContext _context;
 
-        public AccountController( UserManager<AppUser> userManager,SignInManager<AppUser> signInManager,ApplicationDbContext context)
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, ApplicationDbContext context)
         {
             _userManager = userManager;
             _signInManager = signInManager;
@@ -27,18 +27,18 @@ namespace RunGroupWebApp.Controllers
         [HttpPost]
         public async Task<IActionResult> Login(LoginViewModel loginVM)
         {
-            if(!ModelState.IsValid) return View(loginVM);
+            if (!ModelState.IsValid) return View(loginVM);
 
-            var user =await _userManager.FindByEmailAsync(loginVM.EmailAddress);
-            if(user != null)
+            var user = await _userManager.FindByEmailAsync(loginVM.EmailAddress);
+            if (user != null)
             {
                 //user is found
-                var passwordCheck= await _userManager.CheckPasswordAsync(user, loginVM.Password);
-                if(passwordCheck)
+                var passwordCheck = await _userManager.CheckPasswordAsync(user, loginVM.Password);
+                if (passwordCheck)
                 {
                     //password is correct
-                    var result= await _signInManager.PasswordSignInAsync(user, loginVM.Password,false,false);
-                    if(result.Succeeded)
+                    var result = await _signInManager.PasswordSignInAsync(user, loginVM.Password, false, false);
+                    if (result.Succeeded)
                     {
                         return RedirectToAction("Index", "Race");
                     }
@@ -50,6 +50,45 @@ namespace RunGroupWebApp.Controllers
             //user not found
             TempData["Error"] = "Wrong Credentials,Please Try Again";
             return View(loginVM);
+        }
+
+        public IActionResult Register()
+        {
+            var response = new RegisterViewModel();
+            return View(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel registerVM)
+        {
+            if (!ModelState.IsValid) return View(registerVM);
+
+            var user = await _userManager.FindByEmailAsync(registerVM.EmailAddress);
+            if (user != null)
+            {
+                TempData["Error"] = "This email is already in use";
+                return View(registerVM);
+            }
+            var newUser = new AppUser
+            {
+                Email = registerVM.EmailAddress,
+                UserName = registerVM.EmailAddress,
+
+            };
+            var newUserResponse= await _userManager.CreateAsync(newUser,registerVM.Password);
+
+            if (newUserResponse.Succeeded)
+            {
+                await _userManager.AddToRoleAsync(newUser, UserRoles.User);
+            }
+            return RedirectToAction("Index","Race");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index","Race");
         }
     }
 }
